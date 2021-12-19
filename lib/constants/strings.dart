37865d0d07e07125abdccf7db292de25d8a46a07
toubal77 <72252709+toubal77@users.noreply.github.com>
2021-12-19 17:15:22 +0100

    add erreur string

--- lib/constants/strings.dart
+++ lib/constants/strings.dart
@@ -1,12 +1,12 @@
 const String localUserInfoKey = 'userInfo';
-const String wrongNameError = '';
-const String invalidUsernameSignUpError = '';
-const String invalidUsernameSignInError = '';
-const String invalidWilayaError = '';
+const String wrongNameError = 'Veuillez entrer un nom valide';
+const String invalidUsernameSignUpError = 'Veuillez entrer un user name valide';
+const String invalidUsernameSignInError = 'Veuillez entrer un user name valide';
+const String invalidWilayaError = 'Veuillez entrer une wilaya valide';
 const String invalidPasswordError = 'Veuillez entrer un mot de passe valide';
 const String invalidPhoneNumberError =
     'Veuillez entrer un numéro de telephone valide';
-const String invalidVerificationCodeError = '';
+const String invalidVerificationCodeError = 'Veuillez entrer un code valide';
 const String invalidActivityError = '';
 const String invalidEmailError = 'Veuillez entrer un email valide';
 const String internetError =
