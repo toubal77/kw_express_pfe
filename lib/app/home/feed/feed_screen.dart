8dc701ecfa6ce2cfc26c1bbadfc2132b33957cd3
toubal77 <72252709+toubal77@users.noreply.github.com>
2021-12-20 18:53:27 +0100

    search field feed

--- lib/app/home/feed/feed_screen.dart
+++ lib/app/home/feed/feed_screen.dart
@@ -51,6 +51,29 @@ class _FeedScreenState extends State<FeedScreen> {
               carouselSliderStream: carouselSliderStream,
               isLoadingNextMessages: isLoadingNextMessages,
             ),
+            Container(
+              height: 50,
+              padding: EdgeInsets.only(top: 7, left: 16, right: 16, bottom: 5),
+              child: TextField(
+                textAlign: TextAlign.center,
+                decoration: InputDecoration(
+                  hintText: "Search...",
+                  hintStyle: TextStyle(color: Colors.grey),
+                  prefixIcon: Icon(
+                    Icons.search,
+                    color: Colors.grey.shade600,
+                    size: 20,
+                  ),
+                  filled: true,
+                  fillColor: Colors.grey.shade300,
+                  contentPadding: EdgeInsets.all(8),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                    borderSide: BorderSide(color: Colors.grey.shade300),
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
