2c4f54c32cc6a3a3d7353b83fb1ca432381e669d
toubal77 <72252709+toubal77@users.noreply.github.com>
2022-04-11 17:29:53 +0200

    orderSend notification

--- functions/functions/src/index.ts
+++ functions/functions/src/index.ts
@@ -1,9 +1,17 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+admin.initializeApp();
+
+const db = admin.firestore();
+
+export const orderSend = functions.firestore.document("orders").onUpdate(async (snapshot, context) => {
+  const userId = context.params.userId;
+
+  const querySnapshot = await db.collection("orders").get();
+  querySnapshot.forEach(async (orderDoc) => {
+    console.log(orderDoc.data().createdBy);
+    console.log(orderDoc.data().idResto);
+    console.log(userId);
+  });
 });
